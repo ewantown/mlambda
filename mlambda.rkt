195c032388d07a3799b41edfd727fb67f01e5a4d
ewantown <etown@duck.com>
2025-02-01 04:47:24 -0800

    cleaned up main file

--- mlambda.rkt
+++ mlambda.rkt
@@ -1,21 +1,31 @@
 #lang racket
 
-(define sub# 0) (define uni# 0) (define ret# 0) (define rec# 0) (define lim# 0)
-(define (reset-stats)
-  (set! sub# 0) (set! uni# 0) (set! ret# 0) (set! rec# 0))
-(define (print-stats) 
-  (print (list 'subs# sub# 'uni# uni# 'ret# ret# 'rec# rec#)))
+;; Call this language M-λ, i.e. "meta-lambda"
+
+;; _Syntax:_                    .  _Semantics:_
+;; <sym>   ≡ x: (symbol? x)     .      
+;; <wff> ::= (-> <sym> <wff>)   .  := abstraction
+;;         | (<wff> <wff>)      .  := application (call-by-name)
+;;         | - - - - - - - - -  .
+;;         | (□ <wff>)          .  := quotation
+;;         | (▷ <wff> <wff>)    .  := quote-combination - the "Kleene" operator
+;;         | (ε <wff>)          .  := evaluation
+;;         | - - - - - - - - -  .
+;;         | (ρ <wff>)          .  := "reset" (continuation delimiter)
+;;         | (σ <wff>)          .  := "shift" (delim. continuation introducer)
+;;         | (callcc <wff>)     .  := "control" (global continuation introducer)
+;;         | (dropcc <wff>)     .  := "abort"   (global continuation eliminator)
+
 
 (define (meval sexp)
+  "Interpreter for M-λ"
   (letrec ((sub (lambda (a x e)
-                  "Substitute a for x in e."
-                  (begin (set! sub# (add1 sub#)))
+                  "Substitute 'a' for 'x' in 'e'."
                   (match e
-                    ;; Implicit definition of syntax
                     [`(callcc ,f) `(callcc ,(sub a x f))]
                     [`(dropcc ,t) `(dropcc ,(sub a x t))]
                     [`(ρ ,p)    `(ρ ,(sub a x p))]
-                    [`(σ ,f)    `(σ ,(sub a x f))]                                        
+                    [`(σ ,f)    `(σ ,(sub a x f))]
                     [`(□ ,p)    `(□ ,(sub a x p))]
                     [`(▷ ,p ,q) `(▷ ,(sub a x p) ,(sub a x q))]
                     [`(ε ,p)    `(ε ,(sub a x p))]
@@ -23,8 +33,7 @@
                     [`(,l ,r)   `(,(sub a x l) ,(sub a x r))]
                     [_           (if (eq? e x) a e)])))
            (ret (λ (v bound? cc)
-                  "Apply continuation to value v (i.e. return)."
-                  (begin (set! ret# (add1 ret#)))
+                  "Apply continuation to value v."
                   (match cc
                     ['• v]
                     [`((ε •) . ,k)
@@ -41,28 +50,26 @@
                        (rec (sub r x b) (λ (y) (or (eq? x y) (bound? y))) k))]
                     [_ (error "Ill-formed continuation:" cc)])))
            (rec (λ (e bound? cc)
-                  "Interpret expression e in the current context."
-                  (begin (set! rec# (add1 rec#)))
-                  ;; (trace e cc)
+                  "Substitutionally interpret e in the current context."
                   (match e
-                    ;; Continuations
+                    ;; Delimited continuation handling
+                    [`(ρ ,e) (rec e bound? `((ρ •) . ,cc))]
+                    [`(σ ,f)
+                     (let* [(• (gensym))
+                            (u (sub • '• (car (lim cc))))]
+                       (rec `(,f (→ ,• ,u)) bound? (cdr (lim cc))))]
+                    
+                    ;; Global continuation handling
                     [`(callcc ,f)
                      (let* [(• (gensym))
                             (u (sub • '• (uni cc)))]
                        (rec `(,f (→ ,• ,u)) bound? '•))]
                     [`(dropcc ,t) (rec `(callcc (→ ,(gensym) ,t)) bound? cc)]
 
-                    ;; Delimited continuations
-                    [`(ρ ,e) (rec e bound? `((ρ •) . ,cc))]                    
-                    [`(σ ,f)
-                     (let* [(• (gensym))
-                            (u (sub • '• (car (lim cc))))]
-                       (rec `(,f (→ ,• ,u)) bound? (cdr (lim cc))))]                                          
-                    
                     ;; Self-interpreter
                     [`(ε ,t) (rec t bound? `((ε •) . ,cc))]
-                    
-                    ;; Code values                    
+
+                    ;; Code values
                     [`(□ ,t) (ret `(□ ,t) bound? cc)]
                     [`(▷ ,l ,r) (rec l bound? `((▷ • ,r) . ,cc))]
 
@@ -73,64 +80,64 @@
                     [`(,l ,r) (rec l bound? `((• ,r) . ,cc))]
                     [_ (error "Ill-formed expression: " e)])))
            (lim (λ (cc)
-                  "Delimit current continuation"
-                  (begin (set! lim# (add1 ret#)))
+                  "Delimit continuation"
                   (match cc
                     ['• `(• . •)]
                     [`(,e . ((ρ •) . ,k)) `(,e . ,k)]
                     [`(,e . ,k) (lim k)]
                     [_ (error "Ill-formed continuation:" cc)])))
            (uni (λ (cc)
-                  "Collapse current continuation into 1-place-open expression."
-                  (begin (set! uni# (add1 uni#)))
+                  "Collapse continuation into one-place-open expression."
                   (match cc
                     ['• '•]
                     [`(,e . ,k) (sub e '• (uni k))]
-                    [_ (error "Ill-formed continuation:" cc)])))           
+                    [_ (error "Ill-formed continuation:" cc)])))
            (trace (λ (e cc)
                     (newline) (newline)
                     (print (uni cc)) (newline) (newline)
                     (print e) (newline) (newline))))
     (rec sexp (λ (_) #f) '•)))
 
-;; Code combinators
+;; Code combinators - cf. extended Curry-Howard, modal logic S4:
 ;; (→ x (→ y (▷ x y)))      :: □(A -> B) -> (□A -> □B) =: axiom K
 ;; (→ x (□ x))              ::  A -> □A                =: axiom N (when x :: A)
 ;; (→ x (□ x))              :: □A -> □□A               =: axiom 4 (when x :: □A)
 ;; (→ x (ε x))              :: □A -> A                 =: axiom T
 
-;; This method, passing a defunctionalized continuation as list of one-place
+;; Commentary:
+;;
+;; The method of passing a defunctionalized continuation as list of one-place
 ;; open expressions, then "rolling it up" into a single closed expression,
 ;; gives us one representation, for both the abstract syntax tree and
 ;; a self-interpreter-determined path from the root of the abstract syntax tree
 ;; (or delimiter) to a point in the abstract syntax (sub)tree.
-;; We then make this representation available in both object and meta system.
+;; We then make this representation available in both object and meta systems.
 
-;; Meta-theoretically, we use the path-at-a-point to make the evaluation
-;; strategy of the object-system self-interpreter independent of the evaluation
-;; strategy of the (defining) metalanguage (racket) interpreter.
+;; Meta-theoretically, we use the defunctionalized path-at-a-point to make the
+;; evaluation strategy of the object-system self-interpreter independent of the
+;; evaluation strategy of the (defining) metalanguage (i.e. Racket) interpreter.
 
-;; By binding a metatheoretic variable to the end of a path, and making
-;; the variable available as parameter to an object-system representation
-;; of the path as a lambda term, we make the path to any given point in
-;; the computation available within the system at that point, as a "backward"
-;; path from the point to the root (delimiter) of the abstract syntax (sub)tree.
+;; By binding a meta-theoretic variable to the open end of a given path, and
+;; making the variable available as parameter to an object-system representation
+;; of the path as a lambda term, we make the path (to any given point) in
+;; the computation available within the system (at that point), as a "backward"
+;; path from the point (delimiter) to the root of the abstract syntax (sub)tree.
 
 ;; That enables a node in the object system AST to encode a program which
 ;; captures the position of the node in the flow of execution, as a path through
 ;; the AST, then via its children independently determines the root of the AST,
-;; effectively specifying the next instruction to execute. Subtrees of such a
-;; node (in the body of the function supplied to callcc) may "return" by
-;; invoking the parameter-supplied path-cum-function on a value, or they may
-;; "jump" by not invoking the path-cum-function at all. 
-;; reinstate the captured root as root - ie. completing the path with a value at
-;; the capture-point, thus "returning" a value to that point.
+;; by effectively specifying the next instruction to execute. Subtrees of such
+;; a global continuation node (in the body of the abstraction given to callcc), 
+;; "continue" by applying the path-cum-function-parameter, or simply
+;; "jump" by never invoking the path-cum-function-parameter at all.
 
 
 (define-syntax test
   (syntax-rules ()
     [(_ name prop) (when (not prop) (error "Test failed: " name))]))
 
+;; Staged Quines
+;; Squine0 is not a Quine in M-λ, but its value (Squine1) is
 (define squine0 '((→ x (▷ x (□ x))) (□ (→ x (▷ x (□ x))))))
 (define squine1 '(□ ((→ x (▷ x (□ x))) (□ (→ x (▷ x (□ x)))))))
 (test 'squine-test-0 (not (equal? squine0 (meval squine0))))
@@ -138,51 +145,38 @@
 (test 'squine-test-2 (equal? squine1 (meval (meval squine1))))
 (test 'squine-test-3 (equal? squine1 (meval `(ε ,squine1))))
 
-;; Object-lang
+
+;; Basic encoding of arithmetic in object system
 (define zero '(→ f (→ x x)))
-  
+
 (define next '(→ n (→ f (→ x (f ((n f) x))))))
-  
+
 (define prev '(→ n (→ f (→ x (((n (→ g (→ h (h (g f))))) (→ o x)) (→ i i))))))
 
 (define iszero '(→ f ((f (→ z (→ x (→ y y)))) (→ x (→ y x)))))
-  
+
 (define ifzero `((→ p (→ a (→ b ((p a) b)))) ,iszero))
 
-(define Y '(→ f ((→ x (f (x x))) (→ x (f (x x))))))
+(define Y '(→ f ((→ g (f (g g))) (→ g (f (g g))))))
 (define plus `(→ f (→ n (→ m (((,ifzero n) m) ((f (,prev n)) (,next m)))))))
-
 (define addo `(,Y ,plus))
-(define addo2 `(ε (▷ (□ ,Y) (□ ,plus))))
-(define addo3 `(((→ y (→ x (ε (▷ (□ y) (□ x))))) ,Y) ,plus))
-
-;; □(□A -> A) -> A
-(define sfix '(→ e ((→ x ((ε x) x)) (▷ (□ (→ t (→ y (t (▷ y (□ y)))))) e))))
-
-;; □(N -> (N -> N)) -> (N -> (N -> N))
-(define splus `(→ e (→ n (→ m (((,ifzero n) m) (((ε e) (,prev n)) (,next m)))))))
-;; ... applying the rule requires interpreting an expression for the rule
-
-;; N -> (N -> N)
-(define saddo `(,sfix (□ ,splus)))
-                    
-
-(define addm (λ (i j) (number (meval `((,addo ,(numeral i)) ,(numeral j))))))
 
 
-;;(define infer `(→ e (→ p (→ q (→ k (((ε e) (□ (p q))) (□ q)))))));; ??? 
-;;(define inference `(,sfix (□ ,infer))) ;; ???
+;; Staged recursion, via sfix construct from Kleene second fixpoint theorem
+(define sfix ;; □(□A -> A) -> A
+  '(→ e ((→ x ((ε x) x)) (▷ (□ (→ t (→ y (t (▷ y (□ y)))))) e))))
 
-;; Meta-lang
+(define splus ;; □(N -> (N -> N)) -> (N -> (N -> N))
+  `(→ e (→ n (→ m (((,ifzero n) m) (((ε e) (,prev n)) (,next m)))))))
+;; Notice: applying the rule requires interpreting an expression for the rule
+
+(define saddo ;; N -> (N -> N)
+  `(,sfix (□ ,splus)))
+
+
+;; Some meta-linguistic utilities
 (define numeral
   (λ (n) (if (< 0 n) `(,next ,(numeral (sub1 n))) zero)))
-
-(define translate
-  (λ (s) (match s
-           [`(□ ,t) `,t]
-           [`(→ ,x ,b) `(λ (,x) ,(translate b))]
-           [`(,l ,r) `(,(translate l) ,(translate r))]           
-           [_ s])))
 
 (define number
   (λ (e)
@@ -190,7 +184,22 @@
         e
         (((eval (translate (meval e))) (λ (x) (add1 (number x)))) 0))))
 
-;; Example for callcc
+(define translate
+  (λ (s) (match s
+           [`(□ ,t) `,t]
+           [`(→ ,x ,b) `(λ (,x) ,(translate b))]
+           [`(,l ,r) `(,(translate l) ,(translate r))]
+           [_ s])))
+
+(define addm  (λ (i j) (number (meval `((,addo ,(numeral i)) ,(numeral j))))))
+(define saddm (λ (i j) (number (meval `((,saddo ,(numeral i)) ,(numeral j))))))
+
+
+;; Tests
+
+(test 'adding-test1 (= 5 (addm 1 4) (saddm 1 4)))
+(test 'adding-test2 (= 12 (addm 5 7) (saddm 8 4)))
+
 (test 'dropcc-test
       (let [(sexp `(((,ifzero (((,ifzero ,(numeral 1))
                                 ,zero)
